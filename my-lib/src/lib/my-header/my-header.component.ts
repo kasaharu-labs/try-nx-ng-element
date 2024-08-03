90c7c8fdd27198d38a97549d04634ed2a40e9377
Wataru.Kasahara <kasahara1019@gmail.com>
2024-08-03 11:53:03 +0900

    npx nx generate @schematics/angular:component --name=my-header --project=my-lib --style=scss --displayBlock=true

---
+++ my-lib/src/lib/my-header/my-header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-my-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './my-header.component.html',
+  styleUrl: './my-header.component.scss'
+})
+export class MyHeaderComponent {
+
+}
